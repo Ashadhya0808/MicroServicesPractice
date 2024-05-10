2eede0bcad7b3a126be9ee5f4683428b92bbe72d
Ashadhya0808 <167255851+Ashadhya0808@users.noreply.github.com>
2024-05-10 17:58:42 -0400

    Update CurrencyConversionConfiguration.java

--- currency-conversion-service/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/config/CurrencyConversionConfiguration.java
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/config/CurrencyConversionConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "currency-exchange", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "currency-exchange-service") // name defined in eureka naming server for currency exchange service
 public interface CurrencyConversionConfiguration {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
